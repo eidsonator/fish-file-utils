d5a295be6c31642d339bedc7e0b7420e3971fc8d
Todd Eidson <eidsonator@gmail.com>
2021-03-13 16:32:42 -0500

    fix newline on copy

--- functions/cpwd.fish
+++ functions/cpwd.fish
@@ -1,8 +1,8 @@
 function cpwd -d ""
-    if which pbcopy
-        pwd | pbcopy
+    if which pbcopy > dev/null
+        pwd | tr -d '\n' | pbcopy
     else if which xclip
-        pwd | xclip
+        pwd | tr -d '\n' | xclip
     else
         echo "Please istall xclip to use cpwd"
     end
